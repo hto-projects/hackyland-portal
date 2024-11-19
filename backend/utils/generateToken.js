ab8355b6fbfe108373e9beef97a5407b58406bb7
Joseph Maxwell <joseph.maxwell@hyland.com>
2024-11-19 18:10:46 -0500

    we try again

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,7 @@ const generateToken = (res, userId) => {
   res.cookie('jwt', token, {
     httpOnly: false,
     secure: true,
-    sameSite: false,
+    sameSite: "None",
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
