4e5f03184133bd32ce88c76336ac5e663ad23a81
Joseph Maxwell <joseph.maxwell@hyland.com>
2024-11-20 16:56:03 -0500

    attempt to fix another dumb thing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -80,6 +80,7 @@ const registerUser = asyncHandler(async (req, res) => {
 const logoutUser = (req, res) => {
   res.cookie('jwt', '', {
     httpOnly: true,
+    sameSite: "None",
     expires: new Date(0),
   });
   res.status(200).json({ message: 'Logged out successfully' });
